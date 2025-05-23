8419ef02bacc08cad191c49ef6378812d21d5a0e
zeroColaa <sinyoung556@gmail.com>
2025-05-23 20:32:31 +0900

    chore: comment 테이블 DDL 추가 및 외래 키 제약 조건 설정
    - comment 테이블 생성
    - author_id, schedule_id 외래 키 추가 (ON DELETE RESTRICT)

--- schedule.sql
+++ schedule.sql
@@ -1,23 +1,44 @@
 /* ---------- AUTHOR 테이블 ---------- */
-CREATE TABLE author (
-id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-username    VARCHAR(30)  NOT NULL UNIQUE,
-email       VARCHAR(255) NOT NULL UNIQUE,
-password    VARCHAR(255) NOT NULL,
-created_at  DATETIME(6),
-modified_at DATETIME(6)
+CREATE TABLE author
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username    VARCHAR(30)  NOT NULL UNIQUE,
+    email       VARCHAR(255) NOT NULL UNIQUE,
+    password    VARCHAR(255) NOT NULL,
+    created_at  DATETIME(6),
+    modified_at DATETIME(6)
 );
 
 /* ---------- SCHEDULE 테이블 ---------- */
-CREATE TABLE schedule (
-  id          BIGINT AUTO_INCREMENT PRIMARY KEY,
-  title       VARCHAR(255) NOT NULL,
-  contents    LONGTEXT,
-  author_id   BIGINT NOT NULL,
-  created_at  DATETIME(6),
-  modified_at DATETIME(6),
-  CONSTRAINT fk_schedule_author
-      FOREIGN KEY (author_id)
-          REFERENCES author(id)
-          ON DELETE RESTRICT
+CREATE TABLE schedule
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(255) NOT NULL,
+    contents    LONGTEXT,
+    author_id   BIGINT       NOT NULL,
+    created_at  DATETIME(6),
+    modified_at DATETIME(6),
+    CONSTRAINT fk_schedule_author
+        FOREIGN KEY (author_id)
+            REFERENCES author (id)
+            ON DELETE RESTRICT
+);
+
+/* ---------- COMMENT 테이블 ---------- */
+CREATE TABLE comment
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    content     TEXT   NOT NULL,
+    author_id   BIGINT NOT NULL,
+    schedule_id BIGINT NOT NULL,
+    created_at  DATETIME(6),
+    modified_at DATETIME(6),
+    CONSTRAINT fk_comment_author
+        FOREIGN KEY (author_id)
+            REFERENCES author (id)
+            ON DELETE RESTRICT,
+    CONSTRAINT fk_comment_schedule
+        FOREIGN KEY (schedule_id)
+            REFERENCES schedule (id)
+            ON DELETE RESTRICT
 );
