4ecd722e20543ed08130d56ea232b336ea5250cc
zeroColaa <sinyoung556@gmail.com>
2025-05-26 14:43:37 +0900

    fix: update commentRepository

--- src/main/java/io/github/zerocolaa/schedulejpa/repository/CommentRepository.java
+++ src/main/java/io/github/zerocolaa/schedulejpa/repository/CommentRepository.java
@@ -3,10 +3,12 @@ package io.github.zerocolaa.schedulejpa.repository;
 import io.github.zerocolaa.schedulejpa.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByScheduleId(Long scheduleId);
 
